aee982a72d881e265db593ce26a33281d9621497
mirushi <luubieunghi@gmail.com>
2018-09-21 22:41:26 +0700

    Upload Source Code l?n d?u. Ðã có : + Splash Screen. + Fragment cho t?ng navigation menu.

---
+++ app/src/main/java/travel/uittrser/go9/go9/View/TrangChu/JourneyFragment.java
@@ -0,0 +1,18 @@
+package travel.uittrser.go9.go9.View.TrangChu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import travel.uittrser.go9.go9.R;
+
+public class JourneyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.journey_fragment_layout,container,false);
+    }
+}
